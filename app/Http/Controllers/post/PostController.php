b157c282f4b88ea2d1e91e85950076398524e071
Gilson Alves <gilsonalvesdesouza@hotmail.com>
2020-09-01 21:49:28 -0300

    relacionamento um para muitos

---
+++ app/Http/Controllers/post/PostController.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace LaraDev\Http\Controllers\post;
+
+use Illuminate\Http\Request;
+use LaraDev\Http\Controllers\Controller;
+use LaraDev\Models\Post;
+
+class PostController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $post = Post::find($id);
+
+        echo "<h1><b>Artigo</b></h1>";
+        echo "Titulo: {$post->title}<br>";
+        echo "SubTitulo: {$post->subtitle}<br>";
+        echo "Descrição: {$post->description}<br>";
+
+        $autor = $post->author()->get()->first();
+
+        if($autor){
+            echo "<h1><b>Dados do Autor</b></h1>";
+            echo "Nome: {$autor->name}<br>";
+            echo "Email: {$autor->email}";
+        }
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
